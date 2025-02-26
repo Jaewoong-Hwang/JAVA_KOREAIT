d1a3a49ab73d69a229e44e6d7c9889b9cfe58b77
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-02-26 09:34:18 +0900

    20250226

--- javabook_my/src/ch03/c01TypeChange.java
+++ javabook_my/src/ch03/c01TypeChange.java
@@ -3,8 +3,20 @@ package ch03;
 public class c01TypeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//(자료)형변환
+		//연산시 일치하지 않는 자료형을 일치시키는 작업
+		//자동형변환(암시적 형변환) : 컴파일러에 의해 자동 형변화
+		//강제형변환(명시적 형변환) : 프로그래머에 의해 강제 형변화
+		
+		//자동형변환(=)
+		//'변수 연산처리'시 범위가 넓은 공간에 작은값이 대입되는 경우
+		
+		//ex
+		
+		byte a =10;
+		int b= a;
+		
+		//byte > short,char > int > long > float > double
 	}
 
 }

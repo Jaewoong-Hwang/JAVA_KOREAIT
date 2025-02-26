54cc96a0d167cde77f69a0368990471b0e2c9b2e
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-02-26 14:34:12 +0900

    20250226

--- javabook_my/src/ch04/c02Scanner.java
+++ javabook_my/src/ch04/c02Scanner.java
@@ -1,10 +1,35 @@
 package ch04;
 
+import java.util.Scanner;
+
 public class c02Scanner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+//		System.out.printf("정수 입력 : ");
+//		int num1 = sc.nextInt();
+//		System.out.println("입력된 정수 값 : " + num1);
+//
+//		System.out.print("실수 입력: ");
+//		double num2 = sc.nextDouble();
+//		System.out.println("입력된 실수 값 : " + num2);
+
+//		System.out.print("문자열 입력 : ");
+//		String str1 = sc.next(); // 문자열 입력받기 기능 함수, 띄어쓰기는 포함하지 않는다.
+//		System.out.println("입력된 문자열 : " + str1);
+		
+//		System.out.print("입력: ");
+//		String s1 = sc.next();
+//		String s2 = sc.next();
+//		String s3 = sc.next();
+//		System.out.printf("문자열 : %s %s %s\n", s1, s2, s3);
+
+		
+		System.out.print("문자열 입력(띄어쓰기 포함) : ");
+		String str2 = sc.nextLine();				//띄어쓰기도 문자열로. 엔터키로 반환
+		System.out.println("문자열 : " + str2);
 	}
 
 }

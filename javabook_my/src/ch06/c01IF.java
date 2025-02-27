894eb66370764c4f9fea0a6733f034ded4c299fd
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-02-27 11:54:28 +0900

    20250227

--- javabook_my/src/ch06/c01IF.java
+++ javabook_my/src/ch06/c01IF.java
@@ -2,130 +2,180 @@ package ch06;
 
 import java.util.Scanner;
 
-public class c01IF {
+public class C01IF {
 
 	public static void main(String[] args) {
-
-		// 단순 IF
-
-		Scanner sc = new Scanner(System.in);
-//		System.out.print("나이 : ");
+		
+		//-----------------------
+		//단순 IF	
+		//-----------------------
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("나이 : " );
 //		int age = sc.nextInt();
-//		if (age >= 8)
+//		if(age >= 8)
 //			System.out.println("학교에 다닙니다.");
-//		System.out.println("첫 번째 IF 코드 블럭 종료..");
-//		if (age < 8)
-//			System.out.println("학교에 다니지 않습니다");
-//		System.out.println("두 번째 IF 코드 블럭 종료..");
+//		
+//		System.out.println("첫번째 IF 코드 블럭 종료..");
+//		
+//		if(age<8)
+//			System.out.println("학교에 다니지 않습니다.");
+//		
+//		System.out.println("두번째 IF 코드 블럭 종료..");	
+//		System.out.println("프로그램을 종료합니다");
+//		
+//		sc.close();
+		
+		
+		//-----------------------
+		//IF-ELSE	
+		//-----------------------
+		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("나이 : " );
+//		int age = sc.nextInt();
+//		
+//		if(age >= 8)
+//			System.out.println("학교에 다닙니다.");
+//		else
+//			System.out.println("학교에 다니지 않습니다.");
 //
 //		System.out.println("프로그램을 종료합니다");
-
+//		
+//		sc.close();
 		
-		
-		
-		
-		
-		// 문제
+		//-----------------------
+		//1 문제
+		//-----------------------
 		// 정수 한개값을 입력받아 3의 배수이면 해당 수를 출력하세요
-		// 3의 배수이면서 5의 배수라면 출력
-
-//		System.out.println("정수 하나를 입력 해주시면, 3의 배수 또는 5의 배수인지 확인시켜 드립니다.");
+		// 3의 배수이면서 5의 배수라면 출력 - 
+		// 순서도도 그려보세요 - 
+		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("정수입력 : " );
 //		int num = sc.nextInt();
-//		if (num % 3 == 0) {
-//			System.out.printf("%d 는 3의 배수입니다\n", num);
+//		
+//		if(num%3==0) {
+//			System.out.printf("%d 는 3의 배수입니다\n",num);
 //			
-//			if (num % 5 == 0) {
-//				System.out.printf("%d 는 5의 배수입니다\n", num);
+//			if(num%5==0) {
+//				System.out.printf("%d 는 5의 배수입니다\n",num);
 //			}
-//
-//		}else
-//			System.out.printf("%d 는 3과 5의 배수가 아닙니다\n", num);
-		
-		
-		
-		//문제
-		//두 개의 정수를 입력받아 큰 수 출력
-//		System.out.println("정수 두 개를 입력 해주시면, 두 수를 비교해서 더 큰 수를 출력해드립니다.");
-//		System.out.println("정수 입력 :");
-//		int num1 = sc.nextInt();
-//		System.out.println("정수 입력 :");
-//		int num2 = sc.nextInt();
-//		
-//		if(num1 > num2) {
-//			System.out.printf("%d 가(이) %d 보다 더 큰 수 입니다.", num1, num2);
-//			
-//		}else if(num1 < num2)
-//			System.out.printf("%d 가(이) %d 보다 더 큰 수 입니다.", num2, num1);
-//		----------------------------------------------------------------------------
-		
-		//문제 3	==> 세 개의 수가 나온다면 공식 ===> 하나의 공간을 만들어줘야 함
-//		System.out.println("정수 세 개를 입력 해주시면, 두 수를 비교해서 더 큰 수를 출력해드립니다.");
-//		System.out.println("정수 입력 :");
-//		int num1 = sc.nextInt();
-//		System.out.println("정수 입력 :");
-//		int num2 = sc.nextInt();
-//		System.out.println("정수 입력 :");
-//		int num3 = sc.nextInt();
-//		
-		
-//		if(num1 > num2 && num1 > num3) {
-//			System.out.printf("%d 가(이) %d 와(과) %d 보다 더 큰 수 입니다.", num1, num2, num3);
-//		}else if(num2 > num1 && num2 > num3) {
-//			System.out.printf("%d 가(이) %d 와(과) %d 보다 더 큰 수 입니다.", num2, num1, num3);
-//		}else if(num3 > num1 && num3 > num2) {
-//			System.out.printf("%d 가(이) %d 와(과) %d 보다 더 큰 수 입니다.", num3, num1, num2);
 //		}
-		
-//		----------------------------------------------------------------------------
-
-		//문제4
-//		System.out.println("정수 세 개를 입력 해주시면, 총합과 평균(소수점 두 번째 까지)을 출력해드립니다.");
-//		System.out.println("정수 입력 :");
-//		int num1 = sc.nextInt();
-//		System.out.println("정수 입력 :");
-//		int num2 = sc.nextInt();
-//		System.out.println("정수 입력 :");
-//		int num3 = sc.nextInt();
 //		
+//		sc.close();
+		
+		
+		//-----------------------
+		//2 문제
+		//-----------------------
+		// 두개의 정수를 입력받아 큰 수 출력
+		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("두개 정수 입력 : ");
+//		int n1 = sc.nextInt();
+//		int n2 = sc.nextInt();
 //		
-//		int sum = num1 + num2 + num3;
-//		double avg = (double)sum / 3;
-//		System.out.printf("%d , %d , %d 의 총합은 : %d\n", num1, num2, num3, sum);
-//		System.out.printf("%d , %d , %d 의 평균은 : %.2f\n", num1, num2, num3, avg);
+//		if(n1>=n2) {
+//			System.out.println("큰수 : " + n1);
+//		}else {
+//			System.out.println("큰수 : " + n2);
+//		}
+//
+//		sc.close();
+//		
+		//-----------------------
+		//3 문제(☆)
+		//-----------------------
+//		// 세 개의 정수를 입력받아 큰 수 출력
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("세 개 정수 입력 : ");
+//		int n1 = sc.nextInt();
+//		int n2 = sc.nextInt();
+//		int n3 = sc.nextInt();
+//		
+//		int max = n1;	//최대값(max)이 n1이라고 가정 시작 - !
+//		
+//		if(max<n2){
+//			max=n2;
+//		}
+//		if(max<n3) {
+//			max=n3;
+//		}
+//
+//		System.out.println("큰 수 : " + max);
+//		sc.close();
 		
-//		----------------------------------------------------------------------------
 
-		 
-		//문제 5
-		//입력한 수가 짝수이고, 3의 배수라면 출력
-		//입력한 수가 홀수이고, 5의 배수라면 출력
+//		// 세 개의 정수를 입력받아 큰 수 출력
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("세 개 정수 입력 : ");
+//		int n1 = sc.nextInt();
+//		int n2 = sc.nextInt();
+//		int n3 = sc.nextInt();
+//			
+//		
+//		//n1이 제일 큰경우(n1>n2 && n1>n3) 
+//		//n2이 제일 큰경우(n2>n1 && n2>n3)
+//		//n3이 제일 큰경우(n3>n1 && n3>n2)
+//		if(n1>=n2 && n1>=n3) 
+//			System.out.println("큰 수 : " + n1);
+//		
+//		else if(n2>=n1 && n2>=n3)
+//			System.out.println("큰 수 : " + n2);
+//		
+//		else
+//			System.out.println("큰 수 : " + n3);
+//	
+//
+//		sc.close();
 		
-		System.out.println("정수 하나를 입력 해주시면, 6의 배수인지 5의 배수인지 확인시켜 드립니다.");
-		System.out.println("정수 입력 :");
-		int num = sc.nextInt();
 		
-		if(num % 2 == 0) {
-			System.out.printf("%d 는(은) 짝수입니다.", num);
-			if(num % 3 ==0) {
-				System.out.printf("%d 는(은) 3의 배수입니다.", num);
-				if(num % 6 ==0) {
-					System.out.printf("%d 는(은) 짝수이면서 3의 배수입니다.", num);
-				}
-				
-			}
-		}else if(num % 2 !=0) {
-			System.out.printf("%d 는(은) 홀수 입니다.", num);
-			if(num % 5 ==0) {
-				System.out.printf("%d 는(은) 5의 배수 입니다.", num);
-			}
-		}
+//		//-----------------------
+//		//4 문제
+//		//-----------------------
+//		// 세개의 정수를 입력받아 해당수의 합과 평균을 출력
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("세 개 정수 입력 : ");
+//		int n1 = sc.nextInt();
+//		int n2 = sc.nextInt();
+//		int n3 = sc.nextInt();
+//		
+//		int sum = n1 + n2 + n3;
+//		double avg = (double)sum/3;
+//		
+//		System.out.println("합 : " + sum + " 평균 : " + avg);
+//		
+//		sc.close();
+		
+		//-----------------------
+		//5 문제
+		//-----------------------
+//		입력한 수가 짝수이고, 3의 배수라면 출력 (n%2==0 && n%3==0)
+//		입력한 수가 홀수이고, 5의 배수라면 출력 (n%2==1 && n%5==0)
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		//01
+//		if(n%2==0 && n%3==0)
+//			System.out.printf("%d 는 짝수이면서 3의배수입니다\n",n);
+//			
+//		if(n%2==1 && n%5==0)
+//			System.out.printf("%d 는 홀수이면서 5의배수입니다\n",n);
+		
+		if(n%2==0) {
 			
-		
-		
-		
+			if(n%3==0)
+				System.out.printf("%d 는 짝수이면서 3의배수입니다\n",n);
+			
+		}else {
+			
+			if(n%5==0)
+				System.out.printf("%d 는 홀수이면서 5의배수입니다\n",n);
+		}
 		
 		sc.close();
-
+		
 	}
 
 }

aae194047804e1f1b900d103c4906df646de3973
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-03-05 10:55:07 +0900

    20250305

--- javabook_my/src/Ch09/C01정보은닉.java
+++ javabook_my/src/Ch09/C01정보은닉.java
@@ -4,12 +4,55 @@ package Ch09;
 
 //접근 한정자
 //멤버/클래스의 접근범위를 제한을 위한 예약어
+//public		:	모든 클래스에서 접근가능
+//private		:	해당 클래스에서만 접근가능
+//protected		:	상속 관계를 가지는 경우에 접근가능
+//default(기본) 	:	동일 패키지에 속한 클래스에서 접근가능
+
+class C01Person{
+	private String name;		//private 접근 한정
+	private int age;
+	String addr;
+	public C01Person(String name, int age, String addr) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.addr = addr;
+	}
+	//getter 함수(데이터를 외부로 반환)
+	public String getName() {    //get뒤에는 카멜표기법으로 대문자
+		return this.name;
+	}
+	//setter 함수(기존데이터를 수정)
+	public void setAge(int age) {
+		this.age =age;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
 
 public class C01정보은닉 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		C01Person person1 = new C01Person("홍길동",55,"대구");
+		System.out.println(person1.getName());
+		person1.setAge(30);
+		
+		
 	}
 
 }
---
+++ javabook_my/src/Ch09/C03This.java
@@ -0,0 +1,32 @@
+package Ch09;
+
+
+
+//This
+//클래스 내에서 사용되는 예약어
+//생성되는 객체의 위치정보(메모리주소-해시코드값)을 확인하는데 사용
+
+//this : 멤버변수와 지역변수 구별에 사용
+//this : 다른생성자 호출 사용
+
+class C03Simple{
+	int x;
+	int y;
+	C03Simple getThis() {
+		return this;
+	}
+}
+
+	
+	
+public class C03This {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		C03Simple ob = new C03Simple();
+		System.out.println(ob);
+		System.out.println(ob.getThis());
+	}
+
+}

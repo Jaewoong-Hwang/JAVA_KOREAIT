9d2f1fc989398726a6e2f6deadcf52548847a4e2
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-03-06 13:33:11 +0900

    20250306

--- javabook_my/src/ch10/C06Ex.java
+++ javabook_my/src/ch10/C06Ex.java
@@ -1,49 +1,46 @@
 package ch10;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
-
-
 public class C06Ex {
-
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-//		System.out.println("5개의 정수 입력");
-//		int[] arr1 = new int[5];
-//		for (int i = 0; i < arr1.length; i++) {
-//
-//			arr1[i] = sc.nextInt();
-//		}
-//		int sum = 0;
-//		for(int el : arr1) {
-//			sum+=el;
-//		}
-//		
-//		
-//		System.out.println("최대값 : " + Arrays.stream(arr1).max().getAsInt());
-//		System.out.println("최소값 : " + Arrays.stream(arr1).min().getAsInt());
-//		System.out.println("전체합 : " + sum);		
-//		System.out.println("----------------");
-		
-		int [][] scores = new int[5][3];
-		for (int i = 0; i < 5; i++) {
-            System.out.println((i + 1) + "번 학생의 점수를 입력하세요 (국어, 영어, 수학): ");
-            for (int j = 0; j < 3; j++) {
-                scores[i][j] = sc.nextInt();
-            }
-        }
+	//5개의 정수값을 입력받아 int형 배열에 저장하고
+	//최대값,최소값,전체합을 출력합니다.
 	
-		int koreasum = 0;
-		for(int korea = 0; korea<scores[5][1];) {
-			koreasum+=korea;
+	//5 명의 학생에 국영수점수를 입력받아 2차원배열 저장
+	//각 학생의 국영수 점수의 합/평균 구하고
+	//각 과목당 합 /평균 구해보세요
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int score[][] = new int[5][3];
+		
+		for(int i=0;i<score.length;i++) {
+			System.out.printf("%d 번째 학생 국영수 입력 : ",i+1);
+			score[i][0] = sc.nextInt(); //i번째학생의 국어점수입력
+			score[i][1] = sc.nextInt(); //i번째학생의 영어점수입력
+			score[i][2] = sc.nextInt(); //i번째학생의 수학점수입력
 		}
-		System.out.println("국어총점 : " + koreasum);
+		
+		//학생별 점수 총점 / 평균
+		for(int i=0;i<score.length;i++) {
+			int sum = score[i][0] +score[i][1]+score[i][2];
+			double avg = (double)sum/3;
+			System.out.printf("%d 번째 학생 총점 : %d 평균 : %f\n",i+1,sum,avg);
+		}
+		
+		//과목별 점수 총점 / 평균
+		int subject_sum[] = new int[3];
+		for(int i=0;i<score[i].length;i++) //국어 , 영어 , 수학 
+		{	
+			for(int j=0;j<score.length;j++) {			
+				subject_sum[i] +=score[j][i];	
+			}
+		}
+		System.out.printf("국어 총점 : %d 국어 평균 : %f\n",subject_sum[0],(double)subject_sum[0]/score.length);
+		System.out.printf("영어 총점 : %d 영어 평균 : %f\n",subject_sum[0],(double)subject_sum[1]/score.length);
+		System.out.printf("수학 총점 : %d 수학 평균 : %f\n",subject_sum[0],(double)subject_sum[2]/score.length);
 		
 		
 	}
-		
-	
-		
 }

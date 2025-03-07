9b17d68232f25e95b66c483139a2e6348e68b3b8
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-03-07 15:31:43 +0900

    20250307

---
+++ javabook_my/src/ch13/C03InterfaceMain.java
@@ -0,0 +1,20 @@
+package ch13;
+
+interface Remocon{
+	void powerOn();
+	void powerOff();
+	
+}
+
+class Tv implements Remocon{
+	
+}
+class Radio implements Remocon{
+	
+}
+
+public class C03InterfaceMain {
+	public static void main(String[] args) {
+		
+	}
+}

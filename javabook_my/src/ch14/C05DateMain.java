21c302ea185eac7cfc8d95480805846c04e48ff7
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-03-10 11:17:44 +0900

    20250310

--- javabook_my/src/ch14/C05DateMain.java
+++ javabook_my/src/ch14/C05DateMain.java
@@ -1,6 +1,7 @@
 package ch14;
 
-import java.util.Calendar;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
 
 public class C05DateMain {
 	public static void main(String[] args) {
@@ -18,19 +19,39 @@ public class C05DateMain {
 		
 		
 		//Calendar
-		Calendar cal = Calendar.getInstance();
-		System.out.println(cal);
-		System.out.println(cal.get(Calendar.YEAR));
-		System.out.println(cal.get(Calendar.MONTH));
-		System.out.println(cal.get(Calendar.DAY_OF_MONTH));
-		System.out.println(cal.get(Calendar.DAY_OF_WEEK)); //요일
-															// 1-7(일-토)
-		System.out.println(cal.get(Calendar.HOUR_OF_DAY));
-		System.out.println(cal.get(Calendar.MINUTE));
-		System.out.println(cal.get(Calendar.SECOND));
+//		Calendar cal = Calendar.getInstance();
+//		System.out.println(cal);
+//		System.out.println(cal.get(Calendar.YEAR));
+//		System.out.println(cal.get(Calendar.MONTH));
+//		System.out.println(cal.get(Calendar.DAY_OF_MONTH));
+//		System.out.println(cal.get(Calendar.DAY_OF_WEEK)); //요일
+//															// 1-7(일-토)
+//		System.out.println(cal.get(Calendar.HOUR_OF_DAY));
+//		System.out.println(cal.get(Calendar.MINUTE));
+//		System.out.println(cal.get(Calendar.SECOND));
 		
 		
-	
+		
 		//LocalDateTime
+		LocalDateTime now = LocalDateTime.now();
+	
+		//연,월,일,시,분,초 가져오기
+		int year = now.getYear();
+		int month = now.getMonthValue();
+		int day = now.getDayOfMonth();
+		int hour = now.getHour();
+		int minute = now.getMinute();
+		int second = now.getSecond();
+		DayOfWeek dayOfWeek = now.getDayOfWeek(); 
+		//결과출력
+		System.out.println("연 : " + year);
+		System.out.println("월 : " + month);
+		System.out.println("일 : " + day);
+		System.out.println("시 : " + hour);
+		System.out.println("분 : " + minute);
+		System.out.println("초 : " + second);
+		System.out.println("요일 : " + dayOfWeek);
+		
+		
 	}
 }
---
+++ javabook_my/src/ch14/C06SimpleDateFormatMain.java
@@ -0,0 +1,7 @@
+package ch14;
+
+public class C06SimpleDateFormatMain {
+	public static void main(String[] args) {
+
+	}
+}

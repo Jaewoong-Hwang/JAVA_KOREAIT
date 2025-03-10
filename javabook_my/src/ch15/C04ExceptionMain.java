1715ad197ebb8366e30875e8786fc7f330588fc1
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-03-10 16:04:49 +0900

    20250310

--- javabook_my/src/ch15/C04ExceptionMain.java
+++ javabook_my/src/ch15/C04ExceptionMain.java
@@ -1,36 +1,40 @@
 package ch15;
 
 public class C04ExceptionMain {
+
 	public static void main(String[] args) {
+
 		String str = null;
 		try {
-		str.toString();
-		}catch(NullPointerException e) {
-			System.out.println("에외발생 : " +e.getMessage());
-		}
-		int arr[] = {10,20,30,};
-		try {
-		arr[5] = 100;
-		}catch(ArrayIndexOutOfBoundsException e) {
-			System.out.println("예외발생 : " + e.getMessage());
-		}
-		try {
-		Integer.parseInt("a1234");
-		}catch(NumberFormatException e) {
-			System.out.println("예외발생 : " + e.getMessage());
-		}
-		Animal animal = new Dog();
-		try {
-		Cat yummi = (Cat)animal;
-		}catch(ClassCastException e) {
-			System.out.println("예외발생 : " + e.getMessage());
-		}
-		fianlly{
-			//자원정리 작업
-			System.out.println("Finally Test...");
+//			str.toString(); // NULL 예외발생
+
+			int arr[] = { 10, 20, 30 };
+//			arr[5] = 100; // Bound Exception 발생
+
+//			Integer.parseInt("a1234"); // Parse Exception발생
+
+			Animal animal = new Dog();
+			//Cat yummi = (Cat) animal; // Cast Exception;
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
 		}
 		
+//		} catch (NullPointerException e) {
+//			System.out.println(e.getMessage());
+//		} catch (ArrayIndexOutOfBoundsException e2) {
+//			System.out.println(e2.getMessage());
+//		} catch (NumberFormatException e3) {
+//			System.out.println(e3.getMessage());
+//		} catch (ClassCastException e4) {
+//			System.out.println(e4.getMessage());
+//		}
+		
+		finally {
+			//자원정리 작업
+			System.out.println("Finally Test..");
+		}
 		
 		System.out.println("HELLOWORLD");
+
 	}
 }
--- javabook_my/src/ch16/C01GenericMain.java
+++ javabook_my/src/ch16/C01GenericMain.java
@@ -1,7 +1,76 @@
 package ch16;
 
-public class C01GenericMain {
-public static void main(String[] args) {
+
+class 호빵재료{
 	
 }
+
+
+class 민트초코 {
+
+	@Override
+	public String toString() {
+		return "민트초코";
+	}
+
+}
+
+class 야채 extends 호빵재료 {
+
+	@Override
+	public String toString() {
+		return "야채";
+	}
+
+}
+
+class 슈크림 extends 호빵재료 {
+
+	@Override
+	public String toString() {
+		return "슈크림";
+	}
+
+}
+
+class 팥 extends 호빵재료 {
+
+	@Override
+	public String toString() {
+		return "단팥";
+	}
+
+}
+
+class 호빵<T extends 호빵재료> {  // 호빵에 들어갈 재료를 선별하기 상위 클래스를 만들고 extends
+	private T meterial;
+
+	호빵(T meterial) {
+		this.meterial = meterial;
+	}
+
+	// toString 재정의
+	@Override
+	public String toString() {
+		return "호빵 [meterial=" + meterial + "]";
+	}
+
+}
+
+public class C01GenericMain {
+	public static void main(String[] args) {
+
+		호빵<팥> ob1 = new 호빵<팥>(new 팥()); // <>다이아몬드 연산자
+		System.out.println(ob1);
+
+		호빵<슈크림> ob2 = new 호빵<슈크림>(new 슈크림());
+		System.out.println(ob2);
+
+		호빵<야채> ob3 = new 호빵<야채>(new 야채());
+		System.out.println(ob3);
+		
+//		호빵<민트초코> ob4 = new 호빵<민트초코>(new 민트초코()); 상위 클래스에 속하지 않아서 컴파일 에러
+//		System.out.println(ob4);
+
+	}
 }

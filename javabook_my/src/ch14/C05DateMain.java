c9e3442707a52797496e723e93bb169ff456224c
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-03-10 10:52:09 +0900

    20250310

--- javabook_my/src/ch14/C05DateMain.java
+++ javabook_my/src/ch14/C05DateMain.java
@@ -1,7 +1,36 @@
 package ch14;
 
+import java.util.Calendar;
+
 public class C05DateMain {
 	public static void main(String[] args) {
-
+			
+		//Date
+//		Date d1 = new Date();
+//		System.out.println(d1);
+//		System.out.println(d1.getYear()+1900);
+//		System.out.println(d1.getMonth()+1);
+//		System.out.println(d1.getDay());//0-6(일-토)
+//		System.out.println(d1.getHours());
+//		System.out.println(d1.getMinutes());
+//		System.out.println(d1.getSeconds());
+//		System.out.println(d1.getTime());
+		
+		
+		//Calendar
+		Calendar cal = Calendar.getInstance();
+		System.out.println(cal);
+		System.out.println(cal.get(Calendar.YEAR));
+		System.out.println(cal.get(Calendar.MONTH));
+		System.out.println(cal.get(Calendar.DAY_OF_MONTH));
+		System.out.println(cal.get(Calendar.DAY_OF_WEEK)); //요일
+															// 1-7(일-토)
+		System.out.println(cal.get(Calendar.HOUR_OF_DAY));
+		System.out.println(cal.get(Calendar.MINUTE));
+		System.out.println(cal.get(Calendar.SECOND));
+		
+		
+	
+		//LocalDateTime
 	}
 }

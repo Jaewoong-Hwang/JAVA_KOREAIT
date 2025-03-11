683e405090c9f857fd0f79e83506704c6420e1f5
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-03-11 10:20:26 +0900

    20250311

--- javabook_my/src/ch16/C04GenericMain.java
+++ javabook_my/src/ch16/C04GenericMain.java
@@ -34,20 +34,28 @@ class Tiger extends Animal {
 public class C04GenericMain {
 
 	// Generic
-	public static <T> void PrintByGeneric(T[] arr) {
+	public static <T extends Animal> void PrintByGeneric(T[] arr) {
 		for (T el : arr) {
 			System.out.println(el);
 
 		}
 
 	}
+	//Object 로도 받을 수 있음 object가 더 느슨한..?
+	public static void PrintByObject(Object [] animal) {
+		for(Object el : animal) {
+			System.out.println(el);
+		}
+	}
 
 	public static void main(String[] args) {
 
 		Tiger arr1[] = { new Tiger("시베리안 호랑이"), new Tiger("신사동호랭이"), new Tiger("86호랑이띠") };
 		PrintByGeneric(arr1);
-		Panda arr2[] = { new Panda("푸바오"), new Panda("래선팬더"), new Panda("쿵푸팬") };
+		Panda arr2[] = { new Panda("푸바오"), new Panda("래선팬더"), new Panda("쿵푸팬더") };
 		PrintByGeneric(arr2);
+		Object arr3[] = { new Panda("푸바오"), new Tiger("tiger"), new Panda("팬더") };
+		PrintByGeneric(arr3);
 
 	}
 }

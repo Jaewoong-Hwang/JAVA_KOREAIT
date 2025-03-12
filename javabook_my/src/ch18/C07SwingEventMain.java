b4b21642834ce1346a5f3d430f961c138212ab7e
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-03-12 11:11:23 +0900

    20250312

--- javabook_my/src/ch18/C07SwingEventMain.java
+++ javabook_my/src/ch18/C07SwingEventMain.java
@@ -1,6 +1,5 @@
 package ch18;
 
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -14,7 +13,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-
+import javax.swing.text.BadLocationException;
 
 class C07GUI extends JFrame implements ActionListener, KeyListener, MouseListener {
 	JButton btn1;
@@ -25,39 +24,38 @@ class C07GUI extends JFrame implements ActionListener, KeyListener, MouseListene
 	JTextArea area1;
 
 	C07GUI(String title) {
-		// Frame
+		// frame
 		super(title);
-		setBounds(100, 100, 330, 310);
+		setBounds(100, 100, 410, 400);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// Panel
 		JPanel panel = new JPanel();
 		panel.setLayout(null);
 
-		// Component
-		area1 = new JTextArea();
-
+		// Component 참조변수명
+		area1 = new JTextArea(); // textarea = area1
+		area1.setBounds(10, 10, 260, 280);
+		// textarea's scroll = scroll1
 		JScrollPane scroll1 = new JScrollPane(area1);
-		scroll1.setBounds(10, 10, 180, 200);
+		scroll1.setBounds(10, 10, 250, 280);
 
-		btn1 = new JButton("파일로 저장");
-		btn1.setBounds(200, 10, 100, 35);
+		btn1 = new JButton("파일로저장");// 파일로저장 : btn1
+		btn1.setBounds(270, 10, 110, 30);
 
-		btn2 = new JButton("1:1 요청");
-		btn2.setBounds(200, 60, 100, 35);
+		btn2 = new JButton("1:1요청");// 1:1요청 : btn2
+		btn2.setBounds(270, 50, 110, 30);
 
-		btn3 = new JButton("대화기록보기");
-		btn3.setBounds(200, 110, 100, 35);
+		btn3 = new JButton("대화기록보기");// 대화기록보기 : btn3
+		btn3.setBounds(270, 90, 110, 30);
 
-		input = new JButton("입력");
-		input.setBounds(200, 220, 100, 35);
+		input = new JButton("입력"); // 입력 : input
+		input.setBounds(270, 300, 110, 30);
 
-		txt1 = new JTextField();
-		txt1.setBounds(10, 220, 180, 35);
+		txt1 = new JTextField(); // textfield : txt1
+		txt1.setBounds(10, 300, 250, 30);
 
-		Font font1 = new Font("맑은 고딕", Font.BOLD, 11);
-
-		// eventListener add
+		// event listener add
 		btn1.addActionListener(this);
 		btn2.addActionListener(this);
 		btn3.addActionListener(this);
@@ -66,124 +64,119 @@ class C07GUI extends JFrame implements ActionListener, KeyListener, MouseListene
 		area1.addMouseListener(this);
 
 		// panel(component)
-		//panel.add(scroll1);
+//		panel.add(area1);
+		panel.add(scroll1);
 		panel.add(btn1);
 		panel.add(btn2);
 		panel.add(btn3);
 		panel.add(input);
 		panel.add(txt1);
-		btn3.setFont(font1);
 
-		// Frame(Panel)
+		// frame(panel)
 		add(panel);
 
-		// Frame
+		// frame
 		setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+
 //		System.out.println("클릭!");
 		if (e.getSource() == btn1) {
 			System.out.println("파일로 저장 클릭");
 		} else if (e.getSource() == btn2) {
-			System.out.println("1:1 요청 클릭");
+			System.out.println("1:1요청 클릭");
 		} else if (e.getSource() == btn3) {
 			System.out.println("대화기록보기");
-		} else if (e.getSource() == input)
+		} else if (e.getSource() == input) {
 			System.out.println("입력");
-
-	}
-
-	public class C06SwingEventMain {
-
-		public static void main(String[] args) {
-			// TODO Auto-generated method stub
-
-			// Component 참조변수명
-			// textarea = area1
-			// textarea's scroll = scroll1
-			// 파일로저장 : btn1
-			// 1:1요청 : btn2
-			// 대화기록보기 : btn3
-			// 입력 : input
-			// textfield : txt1
-			new C07GUI("Chatting Server");
 		}
 
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		// System.out.println("key typed..."+e.getKeyChar());
-		// System.out.println("key typed..."+e.getKeyCode());
+//		System.out.println("keyTyped..."+e.getKeyChar());
+//		System.out.println("keyTyped..."+e.getKeyCode());
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+//		System.out.println("keyPressed..."+e.getKeyChar());	
+//		System.out.println("keyPressed..."+e.getKeyCode());
 		if (e.getSource() == txt1) {
 			if (e.getKeyCode() == 10) {
 				String message = txt1.getText();
 				System.out.println(message);
-				area1.append(message+"\n");
+				area1.append(message + "\n");
 				txt1.setText("");
 			}
 		}
 	}
-	
-	//KeyListener
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// System.out.println("KeyPressed..."+e.getKeyChar());
-		// System.out.println("KeyPressed..."+e.getKeyCode());
-
-	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// System.out.println("keyReleased.."+e.getKeyChar());
-		// System.out.println("keyReleased.."+e.getKeyCode());
+//		System.out.println("keyReleased..."+e.getKeyChar());
+//		System.out.println("keyReleased..."+e.getKeyChar());
 	}
-	
-	
-	//MouseListener
+
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		try {
-		//System.out.println("mouseCliked..."+e.getPoint()); //x,y축에 대한 상대적 정보 제공
-		int offset = area1.viewToModel(e.getPoint());
-		//System.out.println("mouseClocked...offset " + offset);
-		int row = area1.getLineOfOffset(offset);
-		//System.out.println("mouseClocked...row " + row);
-		int startOffset = area1.getLineStartOffset(row);
-		int endOffset = area1.getLineEndOffset(row);
-		//System.out.printf("%d %d\n", startOffset,endOffset);
-		String str = area1.getText(startOffset, endOffset-startOffset);
-		System.out.println(str);
-		
-		
-		}catch(BadLocationException e1) {
+
+//			System.out.println("mouseClicked..." + e.getPoint());
+			int offset = area1.viewToModel(e.getPoint());
+//			System.out.println("mouseClicked...offset : " + offset);
+			int row = area1.getLineOfOffset(offset);
+//			System.out.println("mouseClicked...row : " + row);
+			int startOffset = area1.getLineStartOffset(row);
+			int endOffset = area1.getLineEndOffset(row);
+//			System.out.printf("%d %d\n", startOffset,endOffset);
+			String str = area1.getText(startOffset,endOffset-startOffset );
+			System.out.println(str);
+			
+			
+		} catch (BadLocationException e1) {
+			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+		
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
+}
+
+public class C07SwingEventMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new C07GUI("Chatting UI");
+
+	}
+
 }

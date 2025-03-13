7cb75fb2aa3519f18693e59696d078190e2e5876
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-03-13 15:30:17 +0900

    20250313

--- javabook_my/src/ch19/C05FileInputStreamMain.java
+++ javabook_my/src/ch19/C05FileInputStreamMain.java
@@ -1,6 +1,24 @@
 package ch19;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class C05FileInputStreamMain {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+	
+		InputStream fin = new FileInputStream("C:\\IOTEST\\data.csv");
+		StringBuffer strungBuffet = new StringBuffer();
+		
+		//fin.read()확인
+		while(true) {
+			int data= fin.read();
+			if(data==-1) break;
+			
+			System.out.print((char)data);
+		}
+		fin.close();
+		
+	
 	}
 }

a163a8649cd09326ff81b2fabba4145c7277815d
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-03-14 10:39:38 +0900

    20250314

---
+++ javabook_my/src/ch19/C09URLStreamMain.java
@@ -0,0 +1,44 @@
+package ch19;
+
+import java.io.BufferedInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class C09URLStreamMain {
+	public static void main(String[] args) throws URISyntaxException, IOException {
+		
+		
+		URL url =( new URI("https://n.news.naver.com/mnews/article/469/0000853552")).toURL();
+		InputStream in = url.openStream();
+		
+		//보조 스트림 추가(ch20에서 수업)	
+		BufferedInputStream buffIn = new BufferedInputStream(in);	//버퍼 공간 추가
+		Reader rin = new InputStreamReader(buffIn); //바이트 단위를 캐릭터로 가져올떄				//byte ->char 변환
+		
+		Writer out = new FileWriter("C:\\IOTEST\\index.html");
+		
+		
+//		OutputStream out = new FileOutputStream("C:\\IOTEST\\index.html");
+		while(true) {
+			int data = rin.read();
+			if(data==-1)
+				break;
+			System.out.print((char)data);
+			out.write((char)data);
+			out.flush();
+			
+		}
+		rin.close();
+		buffIn.close();
+		in.close();
+		out.close();
+		
+	}
+}

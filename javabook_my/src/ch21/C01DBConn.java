4ef5cad7a8467bbe07d8d270d98264cfb5774112
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-03-17 12:23:44 +0900

    20250317

--- javabook_my/src/ch21/C01DBConn.java
+++ javabook_my/src/ch21/C01DBConn.java
@@ -7,8 +7,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class C01DBConn {
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
 		//DB CONN DATA
 		String id = "root";
 		String pw = "1234";
@@ -18,19 +18,17 @@ public class C01DBConn {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		
 		//
-		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			System.out.println("Driver Loading Success...");
 			conn = DriverManager.getConnection(url,id,pw);
 			System.out.println("DB CONNECTED...");
-				
 		}catch(Exception e) {
 			e.printStackTrace();
-		}finally{
-			try {conn.close();}catch (SQLException e) {e.printStackTrace();}
+		}finally {	
+			try { conn.close();} catch (SQLException e) {e.printStackTrace(); }
 		}
 	}
+
 }

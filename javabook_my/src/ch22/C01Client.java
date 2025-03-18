b315c69939198e0be82bea12772606f8a0ae7bbb
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-03-18 14:50:57 +0900

    20250318

---
+++ javabook_my/src/ch22/C01Client.java
@@ -0,0 +1,26 @@
+package ch22;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class C01Client {
+	private void mian() throws UnknownHostException, IOException {
+		// TODO Auto-generated method stub
+		Socket client = new Socket("192.168.16.203",7000);
+//		Socket client = new Socket("192.168.16.85",7000);
+
+		InputStream in = client.getInputStream();		
+		DataInputStream din = new DataInputStream(in);
+		String recv = din.readUTF();
+		System.out.println(recv);
+		
+		din.close();
+		in.close();
+		client.close();
+		
+		
+	}
+}

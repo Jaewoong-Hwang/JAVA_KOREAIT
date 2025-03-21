980553639b7e2af02a4852e8a482a1106af03ad7
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-03-21 17:05:11 +0900

    20250321

---
+++ javabook_my/src/ch34/Main.java
@@ -0,0 +1,29 @@
+package ch34;
+
+@CustomAnnotation(value="HELLO WORLD",number=5,isOpen=true)
+class Simple{
+	String v1;
+	int v2;
+	
+	Simple(){
+		CustomAnnotation ref = this.getClass().getAnnotation(CustomAnnotation.class);
+		System.out.println("valu : " + ref.value());
+		System.out.println("number : " + ref.number());
+		System.out.println("isOpen : " + ref.isOpen());
+		this.v1 = ref.value();
+		this.v2 = ref.number();
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Simple [v1=" + v1 + ", v2=" + v2 + "]";
+	}
+	
+}
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
